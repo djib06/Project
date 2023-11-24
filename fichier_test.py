01bbaa1957aa301426512d1e3e361f01885462d6
17luba <146106312+17luba@users.noreply.github.com>
2023-11-24 11:44:20 +0100

    modif test

--- fichier_test.py
+++ fichier_test.py
@@ -1,6 +1,6 @@
 from gestion_employes import Employe
 
-employe1 = Employe('LUBA1-ANG-Merveille', 'Merveille', 25000)
+employe1 = Employe('LUBA1-ANG-Merveille-test', 'Merveille', 25000)
 employe1.show_detail_employ()
 employe1.generate_email_address()
 employe1.increase_salary_percentage(5)
