af18d2d39cbdfbb2bcbd1abd12bbdeba4562944f
DIALLO Djibril (SNCF RESEAU / Directions Techniques Réseau / DGEX SOLUTIONS) <9921823R@commun.ad.sncf.fr>
2023-11-24 10:55:29 +0100

    modif de fichier_test.py

--- fichier_test.py
+++ fichier_test.py
@@ -1,6 +1,6 @@
 from gestion_employes import Employe
 
-employe1 = Employe('LUBA', 'Merveille', 25000)
+employe1 = Employe('LUBA1', 'Merveille', 25000)
 employe1.show_detail_employ()
 employe1.generate_email_address()
 employe1.increase_salary_percentage(5)
